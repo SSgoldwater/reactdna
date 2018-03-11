954795f14ef293d6131a03663acadb5b03fec767
ssgoldwater <stanleyjsdz@gmail.com>
2018-03-10 18:07:53 -0800

    add update route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const neo4j = require('neo4j-driver').v1;
+
 const driver = neo4j.driver('bolt://127.0.0.1:7687', neo4j.auth.basic('neo4j', 'Mapl3l3af!'));
 const session = driver.session();
 
@@ -26,6 +27,20 @@ api.post("/todos/create", (req, res) => {
   ));
 
   resultPromise.then(result => {
+    session.close();
+    res.send(result.records);
+    driver.close();
+  });
+});
+
+api.post("/todos/update/:id", (req, res) => {
+  const updates = req.body.updates;
+
+  const transaction = session.writeTransaction(tx => tx.run(
+    `MATCH (t:Todo) WHERE ID (t) = ${req.params.id} SET t.content = '${updates.content}' RETURN t`
+  ));
+
+  transaction.then(result => {
     session.close();
     res.send(result.records);
     driver.close();
